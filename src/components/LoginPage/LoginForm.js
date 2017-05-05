c3a9dc5be3aaf6e42c5675ec31b415faa9c92ce5
Mark Winterbottom <mark@londonappdeveloper.com>
2017-05-05 08:36:53 +0100

    Formatting

--- src/components/LoginPage/LoginForm.js
+++ src/components/LoginPage/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Panel, Form } from 'react-bootstrap';
+import { Panel, Form, FormGroup, FormControl, Button } from 'react-bootstrap';
 
 
 const divStyle = {
@@ -10,6 +10,8 @@ const divStyle = {
 const panelStyle = {
   backgroundColor: 'rgba(255,255,255,0.5)',
   border: 0,
+  paddingLeft: 20,
+  paddingRight: 20
 }
 
 class LoginForm extends Component {
@@ -19,9 +21,17 @@ class LoginForm extends Component {
       <div style={divStyle}>
         <Panel style={panelStyle}>
           <Form horizontal className="LoginForm">
-            <input type="email" placeholder="Email Address" /> <br />
-            <input type="password" placeholder="password" /> <br />
-            <input type="submit" value="Login" />
+            <FormGroup controlId="formEmail">
+              <FormControl type="email" placeholder="Email Address" />
+            </FormGroup>
+            <FormGroup controlId="formPassword">
+              <FormControl type="password" placeholder="password" />
+            </FormGroup>
+            <FormGroup controlId="formSubmit">
+              <Button type="submit">
+                Login
+              </Button>
+            </FormGroup>
           </Form>
         </Panel>
       </div>
